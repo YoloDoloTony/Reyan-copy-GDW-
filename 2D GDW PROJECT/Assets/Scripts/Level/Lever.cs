39c11a787b8e059f2c5ea7b462d1249b5a2f9462
GavinC600 <gavin.cole@ontariotechu.net>
2022-04-09 09:20:40 -0400

    Fixed lever anim

--- 2D GDW PROJECT/Assets/Scripts/Level/Lever.cs
+++ 2D GDW PROJECT/Assets/Scripts/Level/Lever.cs
@@ -1,45 +1,26 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Lever : MonoBehaviour
 {
+    Animator Animator;
+
     bool isToggled = true;
     bool canInteract;
     public GameObject doorObject;
-    Animator Animator;
-
-    SpriteRenderer SpriteRenderer;
 
     private void Awake()
     {
-        SpriteRenderer = GetComponent<SpriteRenderer>();
         Animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (canInteract && Input.GetKeyDown(KeyCode.E))
         {
             isToggled = !isToggled;
             doorObject.SetActive(isToggled);
-            Animator.SetBool("flipped", isToggled);
+            Animator.SetBool("triggered", isToggled);
         }
-
-        /*if (isToggled)
-        {
-            SpriteRenderer.color = Color.green;
-        }
-        else
-        {
-            SpriteRenderer.color = Color.red;
-            //will change when we have anims
-        }
-        if (canInteract)
-        {
-            SpriteRenderer.color = Color.yellow;
-        }*/
     }
 
     private void OnTriggerEnter2D(Collider2D leverBox)
